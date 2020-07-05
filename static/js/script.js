d2a3e8ad3308527aa97db3f79f07da47db1949d5
iam_j <ydderhtnawsaj@gmail.com>
2020-07-05 17:44:31 +0530

    Dark mode beta

--- static/js/script.js
+++ static/js/script.js
@@ -49,7 +49,16 @@ function search(){
     _st('install','{{site.swiftype.searchId}}','2.0.0');
 }
 
-
+function dark_toggle() {
+    var el1 = document.getElementById("dark-reader");
+    if(el1.disabled) {
+        el1.disabled = false;
+        localStorage.setItem("darkreader", "enabled");
+    } else {
+        el1.disabled = true;
+        localStorage.setItem("darkreader", "disabled");
+    }
+}
 
 
 
